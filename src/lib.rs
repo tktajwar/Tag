c0c79056c1366a8e043915278a0c5d8e17b365f0
Takey Tajwar <tajwar.earth@pm.me>
2025-06-10 12:28:22 +0600

    add ordering test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
 mod tag;
 
 pub use crate::tag::TagID;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ordering() {
+	let a = TagID::from("@1.0");
+	let b = TagID::generate_next(&a);
+	let a1 = TagID::generate_between(&a, &b);
+	let a2 = TagID::generate_between(&a1, &b);
+
+	assert!(a < a1 && a1 < a2 && a2 < b);
+    }
+}
